d0e93463d4588433d09ce1dbaa56743304cdfc95
RoBob4t <sethinoor@gmail.com>
2022-10-24 15:16:16 -0700

    Initial Commit

---
+++ RocketsApp/RocketsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketsAppApp.swift
+//  RocketsApp
+//
+//  Created by Harnoor Sethi on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RocketsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
